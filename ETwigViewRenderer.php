a5ed000be9c22aab6a913d1014a1f6d84a9acdf2
Leonid Svyatov <leonid@svyatov.ru>
2011-10-02 02:04:33 +0400

    Complete code rewrite, a bunch of new features
    * added an ability to add Twig globals (objects and static classes)
    * added an ability to add Twig functions and filters
    * added an ability to specify Twig location through path alias
    * added an ability to change templates syntax
    * default template extension changed to '.twig'
    * Yii::app() object now can be accessed in templates by name 'App' ({{ App.cache.get('id') }})

--- ETwigViewRenderer.php
+++ ETwigViewRenderer.php
@@ -2,99 +2,273 @@
 /**
  * Twig view renderer
  *
+ * @author Leonid Svyatov <leonid@svyatov.ru>
  * @author Alexander Makarov <sam@rmcreative.ru>
- * @link http://code.google.com/p/yiiext/
- * @link http://www.twig-project.org/
+ * @link http://github.com/yiiext/twig-renderer
+ * @link http://www.twig-project.org
  *
- * @version 0.9.4
+ * @version 1.1.0
  */
-class ETwigViewRenderer extends CApplicationComponent implements IViewRenderer {
-    public $fileExtension='.html';
-
-	/**
-	 * @var array Twig environment options
-	 * @see http://www.twig-project.org/doc/api.html#environment-options	 *
-	 */
-	public $options = array();
-
-	/**
-	 * @var array extension classes list
-	 */
-	public $extentions = array();
-
-    private $twig;
-
+class ETwigViewRenderer extends CApplicationComponent implements IViewRenderer
+{
     /**
-     * Component initialization
+     * @var string Path alias to Twig
      */
-    function init(){
-		if(empty($this->options['charset']))
-			$this->options['charset'] = Yii::app()->charset;
+    public $twigPathAlias = 'application.vendors.Twig';
+    /**
+     * @var string Twig template files extension
+     */
+    public $fileExtension = '.twig';
+    /**
+      * @var array Twig environment options
+      * @see http://twig.sensiolabs.org/doc/api.html#environment-options
+      */
+    public $options = array();
+    /**
+     * @var array Objects or static classes
+     * Keys of array are names to call in template, values - objects or names of static class as string
+     * Example: array('html'=>'CHtml', 'clientScript'=>Yii::app()->clientScript)
+     * Than in template: {{ html.link('Login', 'site/login') }} or {{ clientScript.registerCssFile(...) }}
+     */
+    public $globals = array();
+    /**
+     * @var array Custom functions
+     * Keys of array are names to call in template, values - names of functions or static methods of some class
+     * Example: array('rot13'=>'str_rot13', 'link'=>'CHtml::link')
+     * Than in template: {{ rot13('test') }} or {{ link('Login', 'site/login') }}
+     */
+    public $functions = array();
+    /**
+     * @var array Custom filters
+     * Keys of array are names to call in template, values - names of functions or static methods of some class
+     * Example: array('rot13'=>'str_rot13', 'jsonEncode'=>'CJSON::encode')
+     * Then in template: {{ 'test'|rot13 }} or {{ model|jsonEncode }}
+     */
+    public $filters = array();
+    /**
+     * @var array Custom extensions
+     * Example: array('Twig_Extension_Sandbox', 'Twig_Extension_Text')
+     */
+    public $extensions = array();
+    /**
+     * @var array Twig lexer options
+     * @see http://twig.sensiolabs.org/doc/recipes.html#customizing-the-syntax
+     * Example: Smarty-like syntax
+     * array(
+     *     'tag_comment'  => array('{*', '*}'),
+     *     'tag_block'    => array('{', '}'),
+     *     'tag_variable' => array('{$', '}')
+     * )
+     */
+    public $lexerOptions = array();
 
-        Yii::import('application.vendors.*');
+    private $_twig;
+    private $_basePath;
+    private $_basePathLength;
 
-        // need this since Yii autoload handler raises an error if class is not found
-        spl_autoload_unregister(array('YiiBase','autoload'));
+    function init()
+    {
+        require Yii::getPathOfAlias($this->twigPathAlias).'/Autoloader.php';
+        Yii::registerAutoloader(array('Twig_Autoloader', 'autoload'), true);
 
-        // registering twig autoload handler
-        require_once 'Twig/Autoloader.php';
-        Twig_Autoloader::register();
+        $app = Yii::app();
 
-        // adding back Yii autoload handler
-        spl_autoload_register(array('YiiBase','autoload'));
+        /** @var $theme CTheme */
+        $theme = $app->getComponent('theme');
 
-        // setting cache path to application runtime directory
-		if(empty($this->options['cache']))
-        	$this->options['cache'] = Yii::app()->getRuntimePath().DIRECTORY_SEPARATOR.'views_twig'.DIRECTORY_SEPARATOR;
+        if ($theme === null) {
+            $this->_basePath = $app->getBasePath();
+        } else {
+            $this->_basePath = $theme->getBasePath();
+        }
 
-        // here we are using twig loader
-        $loader = new Twig_Loader_Filesystem($this->getBasePath());
-        $this->twig = new Twig_Environment($loader, $this->options);
+        // Need for extracting basePath from template path
+        $this->_basePathLength = strlen($this->_basePath);
 
-        // Load twig extentions
-		if(!empty($this->extentions)){
-			foreach($this->extentions as $extName){
-                $this->twig->addExtension(new $extName());
-			}
-    	}
+        $loader = new Twig_Loader_Filesystem($this->_basePath);
+        $defaultOptions = array(
+            'autoescape' => false, // false because other way Twig escapes all HTML in templates
+            'auto_reload' => true,
+            'cache' => $app->getRuntimePath() . '/twig_cache/',
+            'charset' => $app->charset,
+        );
+        $this->_twig = new Twig_Environment($loader, array_merge($defaultOptions, $this->options));
 
+        // Adding Yii::app() object to globals
+        $this->_twig->addGlobal('App', $app);
+
+        // Adding custom globals (objects or static classes)
+        if (!empty($this->globals)) {
+            $this->addGlobals($this->globals);
+        }
+        // Adding custom functions
+        if (!empty($this->functions)) {
+            $this->addFunctions($this->functions);
+        }
+        // Adding custom filters
+        if (!empty($this->filters)) {
+            $this->addFilters($this->filters);
+        }
+        // Adding custom extensions
+        if (!empty($this->extensions)) {
+            $this->addExtensions($this->extensions);
+        }
+        // Change lexer syntax
+        if (!empty($this->lexerOptions)) {
+            $this->setLexerOptions($this->lexerOptions);
+        }
+
+        return parent::init();
     }
 
     /**
-	 * Renders a view file.
-	 * This method is required by {@link IViewRenderer}.
-	 * @param CBaseController the controller or widget who is rendering the view file.
-	 * @param string the view file path
-	 * @param mixed the data to be passed to the view
-	 * @param boolean whether the rendering result should be returned
-	 * @return mixed the rendering result, or null if the rendering result is not needed.
-	 */
-	public function renderFile($context,$sourceFile,$data,$return) {
-        // current controller properties will be accessible as {{this.property}}
+     * Renders a view file.
+     * This method is required by {@link IViewRenderer}.
+     * @param CBaseController $context the controller or widget who is rendering the view file.
+     * @param string $sourceFile the view file path
+     * @param mixed $data the data to be passed to the view
+     * @param boolean $return whether the rendering result should be returned
+     * @return mixed the rendering result, or null if the rendering result is not needed.
+     */
+    public function renderFile($context, $sourceFile, $data, $return)
+    {
+        // current controller properties will be accessible as {{ this.property }}
         $data['this'] = $context;
 
-        // check if view file exists
-        if(!is_file($sourceFile) || ($file=realpath($sourceFile))===false)
-            throw new CException(Yii::t('yiiext','View file "{file}" does not exist.', array('{file}'=>$sourceFile)));
+        $sourceFile = substr($sourceFile, $this->_basePathLength);
+        $template = $this->_twig->loadTemplate($sourceFile)->render($data);
 
-		$sourceFile = substr($sourceFile, strlen($this->getBasePath()));
-        $template = $this->twig->loadTemplate($sourceFile);
+        if ($return) {
+            return $template;
+        }
 
-		if($return)
-			return $template->render($data);
-		else
-			echo $template->render($data);
-	}
+        echo $template;
+    }
 
-	/**
-	 * Theme-aware basepath
-	 * @return string
-	 */
-	protected function getBasePath()
-	{
-		if(Yii::app()->theme == null)
-			return Yii::app()->getBasePath();
+    /**
+     * Adds global objects or static classes
+     * @param array $globals @see self::$globals
+     */
+    public function addGlobals($globals)
+    {
+        foreach ($globals as $name => $value) {
+            if (!is_object($value)) {
+                $value = new ETwigViewRendererStaticClassProxy($value);
+            }
+            $this->_twig->addGlobal($name, $value);
+        }
+    }
 
-		return Yii::app()->theme->getBasePath();
-	}
+    /**
+     * Adds custom functions
+     * @param array $functions @see self::$functions
+     */
+    public function addFunctions($functions)
+    {
+        $this->_addCustom('Function', $functions);
+    }
+
+    /**
+     * Adds custom filters
+     * @param array $filters @see self::$filters
+     */
+    public function addFilters($filters)
+    {
+        $this->_addCustom('Filter', $filters);
+    }
+
+    /**
+     * Adds custom extensions
+     * @param array $extensions @see self::$extensions
+     */
+    public function addExtensions($extensions)
+    {
+        foreach ($extensions as $extName) {
+            $this->_twig->addExtension(new $extName());
+        }
+    }
+
+    /**
+     * Sets Twig lexer options to change templates syntax
+     * @param array $options @see self::$lexerOptions
+     */
+    public function setLexerOptions($options)
+    {
+        $lexer = new Twig_Lexer($this->_twig, $options);
+        $this->_twig->setLexer($lexer);
+    }
+
+    /**
+     * Returns Twig object
+     * @return Twig_Environment
+     */
+    public function getTwig()
+    {
+        return $this->_twig;
+    }
+
+    /**
+     * Adds custom function or filter
+     * @param string $classType 'Function' or 'Filter'
+     * @param array $elements Parameters of elements to add
+     * @throws CException
+     */
+    private function _addCustom($classType, $elements)
+    {
+        $classFunction = 'Twig_'.$classType.'_Function';
+
+        foreach ($elements as $name => $func) {
+            $twigElement = null;
+
+            switch ($func) {
+                // Just a name of function
+                case is_string($func):
+                    $twigElement = new $classFunction($func);
+                break;
+                // Name of function + options array
+                case is_array($func) && is_string($func[0]) && isset($func[1]) && is_array($func[1]):
+                    $twigElement = new $classFunction($func[0], $func[1]);
+                break;
+            }
+
+            if ($twigElement !== null) {
+                $this->_twig->{'add'.$classType}($name, $twigElement);
+            } else {
+                throw new CException(Yii::t('yiiext',
+                                             'Incorrect options for "{classType}" [{name}]',
+                                             array('{classType}'=>$classType, '{name}'=>$name)));
+            }
+        }
+    }
+}
+
+/**
+ * Class-proxy for static classes
+ * Needed because you can't pass static class to Twig other way
+ *
+ * @author Leonid Svyatov <leonid@svyatov.ru>
+ * @version 1.0.0
+ */
+class ETwigViewRendererStaticClassProxy
+{
+    private $_staticClassName;
+
+    public function __construct($staticClassName) {
+        $this->_staticClassName = $staticClassName;
+    }
+
+    public function __get($property)
+    {
+        return ${$this->_staticClassName}::$property;
+    }
+
+    public function __set($property, $value)
+    {
+        return (${$this->_staticClassName}::$property = $value);
+    }
+
+    public function __call($method, $arguments)
+    {
+        return call_user_func_array(array($this->_staticClassName, $method), $arguments);
+    }
 }
